b24cb40f6038fdf2064e66057b85e7731dd89144
lanaInt <82150842+lanaInt@users.noreply.github.com>
2026-03-28 19:43:28 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int income = 100000;
+        int spending = 56000;
+        System.out.println("Итого (руб):");
+        System.out.println(income - spending);
+    }
+}
